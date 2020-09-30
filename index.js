2f84881104041d83b89c00e457ea78c239dfdf34
Champion113 <sbalbright19@gmail.com>
2020-09-30 00:22:17 -0400

    clone

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const { type } = require('os');
+// array of questions for user
+const questions = [
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
